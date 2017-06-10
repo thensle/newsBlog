0e2a948f5b522da648c7900cdf6113b5d7dac0e0
Tara Hensle <tara@northwestern.edu>
2017-06-10 09:36:52 -0500

    added routes, models, and interactions with mongoose db

--- models/articles.js
+++ models/articles.js
@@ -1,24 +1,27 @@
 // Dependencies
 var mongoose = require("mongoose");
 
-var Schema = mongoose.Schema;
+module.exports = function(){
+  var Schema = mongoose.Schema;
 
-var ArticleSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
- // In an array, so multiple comments can be associated with each article
-  notes: [{
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }]
-});
+  var ArticleSchema = new Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true,
+      unique: true
+    },
+   // In an array, so multiple comments can be associated with each article
+    comments: [{
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }]
+  });
 
-var Article = mongoose.model("Article", ArticleSchema);
+  var Article = mongoose.model("Article", ArticleSchema);
+};
 
 module.exports = Article;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+//Models
+// var Comment = "./models/comments.js";
+var Article = "./models/articles.js"; 
+
 //Express
 var app = express();
 
@@ -23,6 +27,111 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/newsStories");
+
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+// #### Routes ####
+app.get("/", function(request, response){
+  Article.find({}, function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+     response.render("index", {stories: data});
+    }
+  });
+  
+});
+
+app.get("/saved/articles", function(requestServer, response) {
+  // Webscraping from Reddit's "Not the Onion"
+  request("https://www.reddit.com/r/nottheonion", function(error, response, html) {
+    var $ = cheerio.load(html);
+
+    $("p.title").each(function(i, element) {
+      var stories = {};
+      stories = {
+        title: $(this).text(),
+        link: $(element).children().attr("href")
+      };
+
+      var entry = new Article(stories);
+
+      entry.save(function(err, data) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          // console.log(data);
+        }
+      });
+
+    });
+  });
+  response.send("Scrape Complete");
+});
+
+// JSON of Articles and Associated Comments in db
+app.get("/articles", function(request, response) {
+  Article.find({}, function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      response.json(data);
+    }
+  });
+});
+
+// Route to individual article - page to see comments
+app.get("/articles/:id", function(request, response) {
+  Article.findOne({ "_id": req.params.id })
+  .populate("comments")
+  .exec(function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      response.json(data);
+    }
+  });
+});
+
+
+// Post a comment to a particular article
+app.post("/articles/:id", function(request, response) {
+  var newComment = new Comment(req.body);
+
+  newComment.save(function(error, data) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      Article.findOneAndUpdate({ "_id": req.params.id }, { "comments": doc._id })
+      .exec(function(err, data) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          response.send(data);
+        }
+      });
+    }
+  });
+});
+
 //Server Listener
 app.listen(port, function(error){
   if (error){
@@ -30,33 +139,4 @@ app.listen(port, function(error){
   } else {
     console.log("Listening on port: " + port);
   };
-});
-
-// #### WebScraping ####
-var stories = [];
-
-// Webscraping from Reddit's "Not the Onion"
-request("https://www.reddit.com/r/nottheonion", function(error, response, html) {
-  var $ = cheerio.load(html);
-  stories = [];
-  
-  $("p.title").each(function(i, element) {
-    var onionTitle = $(this).text();
-    var link = $(element).children().attr("href");
-
-    stories.push({
-      title: onionTitle,
-      link: link
-    });
-
-  });
-});
-
-// #### Routes ####
-app.get("/", function(request, response){
-  response.render("index", {stories: stories});
-});
-
-app.get("/articles/:id", function(request, response){
-  response.send("Page here");
 });
